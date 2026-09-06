84fe553fe69e4e3fd6934e87a176baf9762fceea
Pascal Jedicke <pascal.jedicke@icloud.com>
2026-09-06 15:33:03 +0200

    feat(nuxt-website/BlogPost): add date formatter
    Date is now in german format ("1. Aug. 2026")

--- apps/nuxt-website/app/components/BlogPost.vue
+++ apps/nuxt-website/app/components/BlogPost.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import type { CmsCarouselItem } from '~/types/cms'
 
-defineProps<{
+const props = defineProps<{
   item: CmsCarouselItem
 }>()
+
+const formattedDate = computed(() => {
+  if (!props.item.date) return ''
+
+  const date = new Date(props.item.date)
+
+  return new Intl.DateTimeFormat('de-DE', {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric'
+  }).format(date)
+})
 </script>
 
 <template>
@@ -11,7 +24,7 @@ defineProps<{
     :title="item.title"
     :description="item.description"
     :image="item.image.src"
-    :date="item.date"
+    :date="formattedDate"
     :to="item.to"
     target="_blank"
     orientation="horizontal"
